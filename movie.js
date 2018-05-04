1e6e03245adfdce5a63405050e32762214538512
Randall Carlisle <cisolutionsinc@gmail.com>
2018-05-04 09:44:35 -0700

    added event-listeners for buttons: cancel, save, add, show

---
+++ movie.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', ev => {
+    let addButton = document.getElementById('add')
+    addButton.addEventListener('click', showAddMovieForm)
+  
+    let showButton = document.getElementById('show')
+    showButton.addEventListener('click', getMovies)
+  
+    let saveButton = document.getElementById('save')
+    saveButton.addEventListener('click', saveMovieDetails, true)
+  
+    let cancelButton = document.getElementById('cancel')
+    cancelButton.addEventListener('click', event => {
+      getMovies()
+    })
+  
+    getMovies()
+    init()
+  });
+  
+  function init(ev) {
+  
+    $('form').submit(event => {
+      event.preventDefault();
+    });
+  }
